6a7b9d6290d65493ab1e1719cffdf196dbc6f013
Andrew Silluron <asilluron@users.noreply.github.com>
2014-10-07 13:11:30 -0700

    Add bower task to Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
 
+var bower = require('bower');
+var mainBowerFiles = require('main-bower-files');
+
+gulp.task('bower', function() {
+    bower.commands.install()
+        .on('log', function(data) {
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        })
+        .on('end', function() {
+            return gulp.src(mainBowerFiles(), {
+                    base: './bower_components'
+                })
+                .pipe(gulp.dest('app/vendor'));
+        });
+});
+
+
+
 gulp.task('default', function() {
-  // place code for your default task here
+    // place code for your default task here
 });
