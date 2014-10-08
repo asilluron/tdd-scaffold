1023957e743060c824ea9e1c012f73601fcc60c9
Andrew Silluron <asilluron@users.noreply.github.com>
2014-10-07 18:02:16 -0700

    Remove GUtil for now

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,8 @@ var mainBowerFiles = require('main-bower-files');
 
 gulp.task('bower', function() {
     bower.commands.install()
-        .on('log', function(data) {
-            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-        })
         .on('end', function() {
-            return gulp.src(mainBowerFiles(), {
+            return gulp.src(mainBowerFiles({includeDev: true}), {
                     base: './bower_components'
                 })
                 .pipe(gulp.dest('app/vendor'));
